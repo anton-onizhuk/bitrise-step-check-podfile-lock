41886fddc836f0cd310bad5d9fbfa7cb469a4513
Anton Onizhuk <aonyshuk@atimi.com>
2018-12-25 18:41:24 +0200

    no message

--- step.sh
+++ step.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-set -ex
+set -x
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+echo " (?) podfile_lock_path: $podfile_lock_path"
+echo " (?) maifest_lock_path: $maifest_lock_path"
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+envman add --key PODS_OUT_OF_SYNC --value 'true'
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+if [ -s "${podfile_lock_path}" ]; then
+    echo " [!] No file found at path '${podfile_lock_path}'"
+    echo " [!] Expected to find Podfile.lock"
+    echo " [!] Exiting"
+    exit 0
+fi
+if [ -s "${maifest_lock_path}" ]; then
+    echo " [!] No file found at path '${maifest_lock_path}'"
+    echo " [!] Expected to find Manifest.lock"
+    echo " [!] Exiting"
+    exit 0
+fi
+
+diff "${podfile_lock_path}" "${maifest_lock_path}" > /dev/null
+if [ $? != 0 ] ; then
+    echo " [!] The sandbox is not in sync with the Podfile.lock. Run 'pod install' or update your CocoaPods installation."
+    echo " [!] Exiting"
+    exit 0
+fi
+
+echo " (?) SUCCESS"
+echo " (?) Pods are in sync, can skip 'pod install'"
+
+envman add --key PODS_OUT_OF_SYNC --value 'false'
